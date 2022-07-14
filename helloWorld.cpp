537727d60de0b9bf3794682ca606c75f7d1acfea
Dinh Quang Lam <dinhquanglam@MacBook-Pro-cua-DQLam.local>
2022-07-14 09:07:47 +0700

    add all

---
+++ helloWorld.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	int a,b;
+	cout << "Nhạp a: ";
+	cin >> a;
+	cout << "Nhạp b: ";
+	cin >> b;
+
+	cout << "a + b = " << a + b << endl;
+	cout << "a - b = " << a - b << endl;
+	cout << "a * b = " << a * b << endl;
+	cout << "a / b = " << a / b << endl;
+	cout << "a % b = " << a % b;
+	return 0;
+}
